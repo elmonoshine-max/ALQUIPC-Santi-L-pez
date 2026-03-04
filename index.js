592439dc6ecc8e5d8344521a7f5e3c34eaa58bc0
elmonoshine-max <elmonoshine@gmail.com>
2026-03-04 11:20:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,51 +8,72 @@ function facturar() {
 
     let precio = 35000;
 
-
+    
     let letras = /^[A-Za-zÁÉÍÓÚáéíóúñÑ\s]+$/;
 
     if (!letras.test(nombre)) {
-    alert("Solo se permiten letras en el nombre");
-    return;
-}
+        alert("Solo se permiten letras en el nombre");
+        return;
+    }
 
-
-
+    
     if (equipos < 2) {
         alert("Mínimo 2 equipos");
         return;
     }
 
+   
     let id = Math.floor(Math.random() * 1000);
 
+    
     let total = equipos * dias * precio;
 
+    let mensajeTipo = "";
+
+    
     if (tipo == "fuera") {
         total = total + total * 0.05;
+        mensajeTipo = "Incremento del 5% por servicio a domicilio";
     }
 
     if (tipo == "local") {
         total = total - total * 0.05;
+        mensajeTipo = "Descuento del 5% por alquiler en el establecimiento";
     }
 
+    if (tipo == "ciudad") {
+        mensajeTipo = "Sin incremento ni descuento";
+    }
+
+    
     let totalExtra = equipos * extra * precio;
-    totalExtra = totalExtra - totalExtra * (extra * 0.02);
+
+    let descuentoExtra = extra * 0.02;
+
+    if (descuentoExtra > 0.20) {
+        descuentoExtra = 0.20; 
+    }
+
+    totalExtra = totalExtra - totalExtra * descuentoExtra;
 
     total = total + totalExtra;
 
+    
     document.getElementById("resultado").innerHTML =
         "Cliente: " + nombre + "<br>" +
         "ID: " + id + "<br>" +
+        "Tipo de alquiler: " + tipo + "<br>" +
+        mensajeTipo + "<br>" +
         "Equipos: " + equipos + "<br>" +
-        "Días: " + dias + "<br>" +
-        "Extras: " + extra + "<br>" +
-        "Total: $" + total;
+        "Días iniciales: " + dias + "<br>" +
+        "Días adicionales: " + extra + "<br>" +
+        "Total a pagar: $" + total;
 }
 
 
 
 
-
+    
 
 
 
